75f950d24bba490f08634b1b88e211fafa6c60a1
Chris Sig Dev <maxvram@chrisvik.com>
2022-02-27 18:25:58 +1100

    not sure about this approach 	modified:   requirements.txt 	modified:   site/app.py 	deleted:    site/src/routes.py 	new file:   site/static/css/jsonedit.css 	new file:   site/static/img/favicon.ico 	new file:   site/static/js/edwinedit.js 	new file:   site/static/js/jeditable.js 	new file:   site/static/js/jquery-3.1.1.min.js 	new file:   site/static/js/jquery.contextMenu.js 	deleted:    site/static/style.css 	deleted:    site/templates/base.html 	deleted:    site/templates/config.html 	modified:   site/templates/index.html 	deleted:    site/templates/rules.html 	deleted:    site/templates/status.html 	deleted:    site/templates/values.html

--- site/app.py
+++ site/app.py
@@ -1,66 +1,104 @@
-
-import os
+#!/usr/bin/python3
+import time
+import re
+from flask import Flask, request, render_template, jsonify, make_response
+from wtforms import Form, BooleanField, HiddenField, StringField, TextAreaField, widgets, SelectMultipleField, RadioField
+import hashlib
+import re
 import json
-
-from flask import Flask
-from flask import request
-from flask import render_template
-
-HOSTNAME = os.getenv('HOST')
-SIG_PATH = os.getenv('SIGNIFIER')
-SITE_PATH = os.path.join(SIG_PATH, 'site')
-CONFIG_PATH = os.path.join(SIG_PATH, 'cfg')
-DEFAULTS_PATH = os.path.join(SIG_PATH, 'sys', 'config_defaults')
-CONFIG_FILES = {'config':'config.json',
-                'values':'values.json',
-                'rules':'rules.json'}
-config = {}
-values = {}
-rules = {}
-
-
+import os
+import random
+from collections import OrderedDict
+from time import strftime
+# Flask setup
 app = Flask(__name__)
 
 
-@app.route("/")
+SIG_PATH = os.getenv('SIGNIFIER')
+
+try:
+    app_root = os.path.join(os.environ["SIGNIFIER"], 'site')
+except:
+    app_root = "/home/pi/Signifier/site"
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    current_config = get_config('config')
-    default_config = get_config('config', 'default')
-    label_types(default_config)
-    print(json.dumps(default_config))
-    return render_template('index.html',
-                           current_config=current_config,
-                           default_config=default_config)
+    curval = loaddata(app_root + "/edwin.json")
+    return render_template('index.html', curval=json.dumps(curval))
 
+@app.route('/save_edwin', methods=['POST'])
+def save_edwin():
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['the_file']
-        if f is not None:
-            f.save('uploaded_file.txt')
-
-
-def get_config(file:str, *args):
-    if 'default' in args:
-        path = DEFAULTS_PATH
+    output = request.json
+    j_out = json.loads(output, object_pairs_hook=OrderedDict)
+    arc = archivedata()
+    if arc == 0:
+        print("Archive Successful!")
+        result = savedata(app_root + "/edwin.json", j_out)
+        if result == 0:
+            print("Save Successful!")
+            curval = loaddata(app_root + "/edwin.json")
+        else:
+            print("Save Failed!")
+            curval = j_out
     else:
-        path = CONFIG_PATH
-    if file in CONFIG_FILES.keys():
-        with open(os.path.join(path, CONFIG_FILES[file])) as c:
-            try:
-                return json.load(c)
-            except json.decoder.JSONDecodeError:
-                return {}
+        print("Archive Failed!")
+        curval = j_out
+    return render_template('index.html', curval=json.dumps(curval))
+
+def archivedata():
+    retval = 0
+    curtime = strftime("%Y%m%d%H%M%S")
+    archive_file = app_root + "/archive/edwin_" + str(curtime) + ".json"
+    try:
+        curdata = loaddata(app_root + "/edwin.json")
+    except:
+        retval = -1 # -1 can't load file
+    if retval == 0:
+        result = savedata(archive_file, curdata)
+        if result != 0:
+            retval = 1
+    return retval
+
+def fixdata(indata):
+
+    u = indata
+    u = u.replace("&lt;empty&gt;", "")
+    u = u.replace("&gt;", ">")
+    u = u.replace("&lt;", "<")
+    return u
 
 
-def label_types(input:dict):
-    for key, val in input.items():
-        if key != 'param_type':
-            if isinstance(val, dict):
-                label_types(val)
-            else:
-                val_type = type(val).__name__
-                if val_type in ['int', 'float']:
-                    val_type = 'number'
-                input[key] = {'value':val, 'param_type':f'{val_type}'}
+def savedata(jsonfile, jdata):
+    retval = 0
+    try:
+        u = open(jsonfile, 'w')
+        u.write(fixdata(json.dumps(jdata, sort_keys=False, indent=4, separators=(',', ': '))) + "\n")
+        u.close()
+    except:
+        retval = 1
+        print("Error saving json data to %s" % jsonfile)
+    return retval
+
+
+def loaddata(jsonfile):
+    try:
+        u = open(jsonfile, 'r')
+    except:
+        retj = {"error": "file could not be opened"}
+    try:
+        j = u.read()
+        u.close()
+    except:
+        retj = {"error": "file could not be read"}
+    try:
+        retj = json.loads(j, object_pairs_hook=OrderedDict)
+    except:
+        retj = {"error": "json parsing error"}
+    return retj
+
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', debug=True, port=6000)
